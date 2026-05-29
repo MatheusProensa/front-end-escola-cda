d76281c20e58d4ca4850ae241b483e17f335eeee
Matheus Proensa <matheu.proensa@gmail.com>
2026-05-29 13:36:23 -0300

    Descrição da alteração

--- src/pages/Momentos.tsx
+++ src/pages/Momentos.tsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import { Layout, useContact }
+import { Layout, useContact } from "../components/site";
 import { asset } from "../lib/assets";
 
 type Album = { img: string; t: string; date: string; fotos: number; destaque?: boolean };
